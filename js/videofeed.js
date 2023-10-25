168b49e8b3555d9003ad51f473b2b50c3d1cba41
Paula Guevara <paula.guevara91@gmail.com>
2023-10-25 12:00:17 -0300

    trabajando en el responsive

--- js/videofeed.js
+++ js/videofeed.js
@@ -1,4 +1,4 @@
-const apiKey = 'ApiKeyGoesHere';
+const apiKey = 'apiKeyGoesHere';
 const maxResults = 9;
 const searchInput = document.getElementById('search_input');
 
@@ -45,74 +45,11 @@ function createYouTubePlayer(container, videoId) {
   playerIframe.height = '100%';
   playerIframe.allowfullscreen = true;
 
-  // Append the player iframe to the card container
   cardContainer.appendChild(playerIframe);
 
-  // Append the card container to the video grid
   container.appendChild(cardContainer);
 
-  // You can add additional styling or classes to the card container if needed
 }
-
-
-
-
-
-
-//CODE THAT WORKS
-/* const maxResults = 9;
-const randomSearchTerm = '4th grade shorts';
-const maxVideoDuration = 'short';
-
-
-const videoGrid = document.querySelector('.video-grid');
-
-fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${randomSearchTerm}&maxResults=${maxResults}&type=video`)
-  .then(response => response.json())
-  .then(data => {
-    const videoItems = data.items || [];
-
-    videoItems.forEach(item => {
-      const videoId = item.id.videoId;
-      const snippet = item.snippet || {};
-      const thumbnailUrl = snippet.thumbnails?.high?.url || '';
-
-      const videoContainer = document.createElement('div');
-      videoContainer.className = 'video-container';
-
-      const thumbnailImage = document.createElement('img');
-      thumbnailImage.src = thumbnailUrl;
-      thumbnailImage.alt = snippet.title || '';
-
-      videoContainer.appendChild(thumbnailImage);
-
-      videoGrid.appendChild(videoContainer);
-
-      createYouTubePlayer(videoContainer, videoId);
-    });
-  })
-  .catch(error => console.error('Error fetching data from YouTube API:', error));
-
-function createYouTubePlayer(container, videoId) {
-  const player = new YT.Player(container, {
-    videoId: videoId,
-    width: 220,
-    height: 380,
-    playerVars: {
-      autoplay: 0,
-      controls: 1,
-      mute: 0,
-    },
-  });
-}
- */
-
-
-
-
-
-
-
 
 
 
